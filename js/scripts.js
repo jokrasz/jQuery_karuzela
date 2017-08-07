8050a06e49cb0ccba4a4f8b0759d36aaa52ac84f
jokrasz <lagarto@wp.pl>
2017-08-07 21:53:51 +0200

    poprawka nr 1 w js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
+window.onload = function() {
+  console.log('The page is fully loaded.');
+  // the rest of the script
+}
+
 $(document).ready(function() {
-   // Kod wykonywany, gdy DOM jest gotowy
- 	var carouselList = $("#carousel ul");
+   // Kod wykonywany, gdy DOM jest gotowy	
  });
 
 $(function(){	
-	setInterval((changeSlide, 3000){ //every 3 seconds, it performs a function to change the slide
-	 	changeSlides(function(){ 
-	 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide); 
-		});
-	});
-	moveFirstSlide (function(){ 	
+	var carouselList = $("#carousel ul");
+	setInterval(changeSlides, 3000); //every 3 seconds, it performs a function to change the slide
+	function changeSlides(function){ 
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide); 
+	}
+	
+	function moveFirstSlide (function){ 	
 	 	var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-	});
+	}
  });
